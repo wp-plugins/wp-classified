168c323db634cb0a062895236f274f4a37931196
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-12 09:15:16 +0000

    added method html2txt
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/trunk@59336 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/_functions.php
+++ includes/_functions.php
@@ -556,6 +556,78 @@ function _print_ad(){
 }
 
 
+
+function html2text( $badStr ) {
+    //remove PHP if it exists
+    while( substr_count( $badStr, '<'.'?' ) && substr_count( $badStr, '?'.'>' ) && strpos( $badStr, '?'.'>', strpos( $badStr, '<'.'?' ) ) > strpos( $badStr, '<'.'?' ) ) {
+        $badStr = substr( $badStr, 0, strpos( $badStr, '<'.'?' ) ) . substr( $badStr, strpos( $badStr, '?'.'>', strpos( $badStr, '<'.'?' ) ) + 2 ); }
+    //remove comments
+    while( substr_count( $badStr, '<!--' ) && substr_count( $badStr, '-->' ) && strpos( $badStr, '-->', strpos( $badStr, '<!--' ) ) > strpos( $badStr, '<!--' ) ) {
+        $badStr = substr( $badStr, 0, strpos( $badStr, '<!--' ) ) . substr( $badStr, strpos( $badStr, '-->', strpos( $badStr, '<!--' ) ) + 3 ); }
+    //now make sure all HTML tags are correctly written (> not in between quotes)
+    for( $x = 0, $goodStr = '', $is_open_tb = false, $is_open_sq = false, $is_open_dq = false; strlen( $chr = $badStr{$x} ); $x++ ) {
+        //take each letter in turn and check if that character is permitted there
+        switch( $chr ) {
+            case '<':
+                if( !$is_open_tb && strtolower( substr( $badStr, $x + 1, 5 ) ) == 'style' ) {
+                    $badStr = substr( $badStr, 0, $x ) . substr( $badStr, strpos( strtolower( $badStr ), '</style>', $x ) + 7 ); $chr = '';
+                } elseif( !$is_open_tb && strtolower( substr( $badStr, $x + 1, 6 ) ) == 'script' ) {
+                    $badStr = substr( $badStr, 0, $x ) . substr( $badStr, strpos( strtolower( $badStr ), '</script>', $x ) + 8 ); $chr = '';
+                } elseif( !$is_open_tb ) { $is_open_tb = true; } else { $chr = '&lt;'; }
+                break;
+            case '>':
+                if( !$is_open_tb || $is_open_dq || $is_open_sq ) { $chr = '&gt;'; } else { $is_open_tb = false; }
+                break;
+            case '"':
+                if( $is_open_tb && !$is_open_dq && !$is_open_sq ) { $is_open_dq = true; }
+                elseif( $is_open_tb && $is_open_dq && !$is_open_sq ) { $is_open_dq = false; }
+                else { $chr = '&quot;'; }
+                break;
+            case "'":
+                if( $is_open_tb && !$is_open_dq && !$is_open_sq ) { $is_open_sq = true; }
+                elseif( $is_open_tb && !$is_open_dq && $is_open_sq ) { $is_open_sq = false; }
+        } $goodStr .= $chr;
+    }
+    //now that the page is valid (I hope) for strip_tags, strip all unwanted tags
+    $goodStr = strip_tags( $goodStr, '<title><hr><h1><h2><h3><h4><h5><h6><div><p><pre><sup><ul><ol><br><dl><dt><table><caption><tr><li><dd><th><td><a><area><img><form><input><textarea><button><select><option>' );
+    //strip extra whitespace except between <pre> and <textarea> tags
+    $badStr = preg_split( "/<\/?pre[^>]*>/i", $goodStr );
+    for( $x = 0; is_string( $badStr[$x] ); $x++ ) {
+        if( $x % 2 ) { $badStr[$x] = '<pre>'.$badStr[$x].'</pre>'; } else {
+            $goodStr = preg_split( "/<\/?textarea[^>]*>/i", $badStr[$x] );
+            for( $z = 0; is_string( $goodStr[$z] ); $z++ ) {
+                if( $z % 2 ) { $goodStr[$z] = '<textarea>'.$goodStr[$z].'</textarea>'; } else {
+                    $goodStr[$z] = preg_replace( "/\s+/", ' ', $goodStr[$z] );
+            } }
+            $badStr[$x] = implode('',$goodStr);
+    } }
+    $goodStr = implode('',$badStr);
+    //remove all options from select inputs
+    $goodStr = preg_replace( "/<option[^>]*>[^<]*/i", '', $goodStr );
+    //replace all tags with their text equivalents
+    $goodStr = preg_replace( "/<(\/title|hr)[^>]*>/i", "\n          --------------------\n", $goodStr );
+    $goodStr = preg_replace( "/<(h|div|p)[^>]*>/i", "\n\n", $goodStr );
+    $goodStr = preg_replace( "/<sup[^>]*>/i", '^', $goodStr );
+    $goodStr = preg_replace( "/<(ul|ol|br|dl|dt|table|caption|\/textarea|tr[^>]*>\s*<(td|th))[^>]*>/i", "\n", $goodStr );
+    $goodStr = preg_replace( "/<li[^>]*>/i", "\n· ", $goodStr );
+    $goodStr = preg_replace( "/<dd[^>]*>/i", "\n\t", $goodStr );
+    $goodStr = preg_replace( "/<(th|td)[^>]*>/i", "\t", $goodStr );
+    $goodStr = preg_replace( "/<a[^>]* href=(\"((?!\"|#|javascript:)[^\"#]*)(\"|#)|'((?!'|#|javascript:)[^'#]*)('|#)|((?!'|\"|>|#|javascript:)[^#\"'> ]*))[^>]*>/i", "[LINK: $2$4$6] ", $goodStr );
+    $goodStr = preg_replace( "/<img[^>]* alt=(\"([^\"]+)\"|'([^']+)'|([^\"'> ]+))[^>]*>/i", "[IMAGE: $2$3$4] ", $goodStr );
+    $goodStr = preg_replace( "/<form[^>]* action=(\"([^\"]+)\"|'([^']+)'|([^\"'> ]+))[^>]*>/i", "\n[FORM: $2$3$4] ", $goodStr );
+    $goodStr = preg_replace( "/<(input|textarea|button|select)[^>]*>/i", "[INPUT] ", $goodStr );
+    //strip all remaining tags (mostly closing tags)
+    $goodStr = strip_tags( $goodStr );
+    //convert HTML entities
+    $goodStr = strtr( $goodStr, array_flip( get_html_translation_table( HTML_ENTITIES ) ) );
+    preg_replace( "/&#(\d+);/me", "chr('$1')", $goodStr );
+    //wordwrap
+    $goodStr = wordwrap( $goodStr );
+    //make sure there are no more than 3 linebreaks in a row and trim whitespace
+    return preg_replace( "/^\n*|\n*$/", '', preg_replace( "/[ \t]+(\n|$)/", "$1", preg_replace( "/\n(\s*\n){2}/", "\n\n\n", preg_replace( "/\r\n?|\f/", "\n", str_replace( chr(160), ' ', $goodStr ) ) ) ) );
+}
+
+
 function _send_ad(){
     global $_GET, $_POST, $wpdb, $table_prefix, $PHP_SELF, $lang;
 	$wpcSettings = get_option('wpClassified_data');
@@ -579,9 +651,7 @@ function _send_ad(){
 		$mailfrom=$_POST['wpClassified_data'][mailfrom];
 		$mailto=$_POST['wpClassified_data'][mailto];
 		//$message .= $name . "just filled in your comments form. They said:\n" . $msg . "\n\n";
-		$message .= "\n" .$lang['_FRIENDTITLE']. "\n\n";
-		$message .= $lang['_ADDETAIL']."\n" . $msg . "\n\n";
-		$message .= "View Photo and More Details: <a href=\"".get_bloginfo('wpurl')."/?page_id=".$pageinfo["ID"]."&_action=va&asid=".$post->ads_subjects_id."&pstart=".((int)$vars["start"])."\">".$subject."</a>";
+		$message .= "<BR>" .$lang['_FRIENDTITLE']. "<br><br>";
 
 		if (!eregi("^[a-z0-9]+([-_\.]?[a-z0-9])+@[a-z0-9]+([-_\.]?[a-z0-9])+\.[a-z]{2,4}$",   $_POST['wpClassified_data'][mailto])){
 			$sendMsg = $lang['_INVALIDEMAIL'];
@@ -595,8 +665,12 @@ function _send_ad(){
 		
 		if ($sendAd == true) {
 			$displayform = false;
-			$filter = rss_filter($message);
-			if (mail($mailto, $subject, $filter,"From: $mailfrom\n")) {
+
+			$message = $lang['_ADDETAIL']."<BR>" . $msg . "<BR><BR>";
+			$message .= "View Photo and More Details: <a href=\"".get_bloginfo('wpurl')."/?page_id=".$pageinfo["ID"]."&_action=va&asid=".$post->ads_subjects_id."&pstart=".((int)$vars["start"])."\">".$subject."</a><HR>";
+
+  			$txt = html2text($message); 
+			if (mail($mailto, $subject, $txt,"From: $mailfrom\n")) {
 				get_wpc_list($lang['_SEND']);
 			} else {
 				$sendMsg = $lang['_SENDERR'];
