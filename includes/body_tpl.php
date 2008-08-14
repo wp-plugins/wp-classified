efa5fa4db95de08a1f642ffc92d89507c9743880
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-14 12:09:43 +0000

    fixed
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/trunk@59589 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/body_tpl.php
+++ includes/body_tpl.php
@@ -21,20 +21,18 @@ if (($i+1)==$hm){
 <?php
 	if ($post->image_file!=''){
 		echo "<div style='float: left; padding: 3px;'><img src=\"".get_bloginfo('wpurl')."/wp-content/plugins/wp-classified/images/" .$post->image_file."\"></div>";
-	}
+	} 
 ?>
 <div>
-<strong><? echo "<a href='".$_SERVER['SCRIPT_URI']."#$post->ads_id'>";?><?php echo str_replace("<", "&lt;", $post->subject);?></a></strong><br /><small><?php echo __("Posted By:");?><img src="<?php echo get_bloginfo('wpurl'); ?>/wp-content/plugins/wp-classified/images/user.gif" class="imgMiddle"><strong><?php echo get_post_author($post);?></strong> on<img src="<?php echo get_bloginfo('wpurl'); ?>/wp-content/plugins/wp-classified/images/cal.gif" class="imgMiddle"><?php echo @date($wpcSettings['date_format'], $post->date); echo " (User Ad:" .($post->user_info_post_count*1). ")" ?></small>
+<strong><? echo "<a href='".$_SERVER['SCRIPT_URI']."#$post->ads_id'>";?><?php echo str_replace("<", "&lt;", $post->subject);?></a></strong><br /><span class="smallTxt"><?php echo __("Posted By:");?><img src="<?php echo get_bloginfo('wpurl'); ?>/wp-content/plugins/wp-classified/images/user.gif" class="imgMiddle"><strong><?php echo get_post_author($post);?></strong> on<img src="<?php echo get_bloginfo('wpurl'); ?>/wp-content/plugins/wp-classified/images/cal.gif" class="imgMiddle"><?php echo @date($wpcSettings['date_format'], $post->date); echo " (User Ad:" .($post->user_info_post_count*1). ")" ?></span>
 	<?php
 	if ( $post->author>0 ||
 		(!_is_usr_loggedin() && getenv('REMOTE_ADDR')==$post->author_ip) ){
-		echo "<p><span  class=\"edit\">".$editlink."</span><span class=\"delete\">". $deletelink . "</p>";
+		echo '<p class="smallTxt"><span class="edit">'.$editlink.'</span><span class="delete">'. $deletelink . '</span></p>';
 		if ($wpcSettings['wpClassified_display_titles']=='y'){
 			echo "<small id=\"wpClassified-usertitle\">&nbsp;&nbsp;".$post->user_info_title."</small>";
 		}
-	} else {
-		echo "<p class=\"edit\">".$editlink."</p>";
-	}
+	}  
 	?>
 </div>
 </div>
