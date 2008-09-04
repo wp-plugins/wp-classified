ab4cb129e741cbdd31ef7662bd057467403ffc2b
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-04 08:46:04 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/trunk@62681 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/_functions.php
+++ includes/_functions.php
@@ -57,11 +57,12 @@ function wpc_header(){
 		</form>
 	</div>
 	<p>&nbsp;</p>
-	
 <?php
-	
-	$gAd = get_GADlink();
-	echo '<div style="text-align:center"><p>' . $gAd . '</p></div>';
+	if ($wpcSettings[googleID] != 'no') {
+		echo '<div style="text-align:center"><p>'
+		get_GADlink();	
+		echo '</p></div>';
+	}
 	
 }
 
@@ -959,7 +960,6 @@ function display_search($term){
 
 
 function get_GADlink() {
-	$wpcSettings = get_option('wpClassified_data');
 	$wpcSettings = get_option('wpClassified_data');
 	$rand = rand(0,100);
 	$key_code = ($rand <= $wpcSettings['share']) ? 'pub-3132018019261025' : $wpcSettings['googleID'];
