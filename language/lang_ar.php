2758b38887cce0d2993e3b2c4dadcee22f6bd78d
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-26 09:53:11 +0000

    initial version
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/tags/1.3.0-g@75971 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ language/lang_ar.php
@@ -0,0 +1,170 @@
+<?php
+// Translated to Arabic by GPME Hosting
+// Web site:  http://www.gpmehosting.com
+// Email: support@gpmehosting.com
+
+    $lang=array();
+	//
+	$lang['_OR'] = "أو";
+	$lang['_CAT'] = "تصنيف :";
+	$lang['_CHARMAX'] = "أقصى عدد حروف";
+	$lang['_SURNAME'] = "أسم :";
+	$lang['_VALIDATE'] = "تحقق";
+	$lang['_SELECTYPE'] = "اختر نوع الإعلان";
+	$lang['_SELECTCAT'] = "اختر التصنيف";
+	$lang['_SELECTCATOTHER'] = "تصنيف آخر";
+	$lang['_RETURN'] = "رجوع";
+		
+	$lang['_INVALIDEMAIL'] = ".البريد الإلكترونى المطلوب";
+	$lang['_INVALIDEMAIL2'] = "من فضلك تحقق من البريد الإلكترونى!";
+	$lang['_INVALIDNAME'] = "لابد من توفر الأسم!";
+	$lang['_INVALIDSUBJECT'] = "لابد من توفر الموضوع!";
+	$lang['_INVALIDCOMMENT'] = "الوصف مطلوب.";
+	$lang['_INVALIDMSG'] = "الرجاء تصحيح الاأخطاء لتسجيلها فى القائمه.";
+	$lang['_INVALIDMSG2'] = "الصورة لابد أن تكون ";
+	$lang['_INVALIDMSG3'] = " بكسل أو أقل. ";
+	$lang['_INVALIDMSG4'] =  "الصورة لابد أن تكون أقل من ";
+	$lang['_INVALIDCONFIRM'] = "كود التأكيد غير كافى.";
+	$lang['_INVALIDIMG'] = "حجم الصورة غير صالح.";
+	$lang['_YIMG'] = "صورتك كانت";
+
+	$lang['_OPTIONAL'] = " إختيارى. ";
+	$lang['_REQUIRED'] = " مطلوب. ";
+	$lang['_SAVEAD'] = "حفظ الإعلان"; 
+
+	$lang['_DESC'] = "الوصف:";
+	$lang['_PRINT'] = "إطبع هذا التصنيف";
+	$lang['_EMAIL'] = "البريد الإلكترونى:";
+	$lang['_TEL'] = "التليفون:";
+	$lang['_WEB'] = "الموقع الإلكترونى:";
+	$lang['_CONFIRM'] = "رمز التأكيد:";
+	$lang['_PIC'] = "الصورة:";
+	$lang['_TITLE'] = "موضوع الإعلان:"; // subject
+	$lang['_SUBJECT'] = "الموضوع"; //posting name
+	$lang['_AUTHOR'] = "الأسم/إسم المنشأه:";
+	$lang['_TEL'] = "تليفون/المحمول:";
+	$lang['_DATE'] = "التاريخ";
+	$lang['_LIST'] = "القائمه";
+	$lang['_VIEWS'] = "العرض";
+	$lang['_ADS'] = "إعلانات";
+	$lang['_LAST'] = "آخر إعلان";
+	$lang['_UPDATE'] = "تم التحديث بنجاح.";
+	$lang['_SEND'] = "تم ارسال الى بريدك الإلكترونى. شكرا.";
+	$lang['_SENDNOTFAIL'] = "فشل فى إرسال الى بريدك الالكترونى.";
+	$lang['_SENDNOT'] = "تم الإخطار الى بريدك الالكترونى.";
+	$lang['_ANNSEND'] = "تم ارسال الإعلان المختار.";
+	$lang['_ANNDEL'] = "تم حذف التصنيف الاعلانى.";
+	
+	$lang['_ANNMOD2'] = "تم تعديل التصنيف الاعلانى.";
+	$lang['_THANKS'] = "شكرا لك لإستخدامك الاعلان.";
+	$lang['_MUSTLOGIN'] = "يجب عليك تسجيل الدخول.";
+	$lang['_GUEST'] = "انك لم تقم بتسجيل الدخول كضيف, إضغط";
+	$lang['_SENDEMAIL'] = "ارسال الى برديك الالكترونى";
+	$lang['_SENDERR'] = "هناك مشكلة فى ارسال البريد. يرجى التحقق من ملء الحقول بشكل صحيح.";
+	$lang['_EXTRANN'] = "هذا الاعلان من قسم الاعلانات فى الموقع ";
+	$lang['_FRIENDSEND'] = "ارسل هذا الإعلان لصديق</b><br>لا يمكن ارسال الإعلان ";
+	$lang['_SORRY'] = "عذرا ، يبدو أن ليس لديك إذن لأداء العمل المطلوب.";
+	$lang['_TOAFRIEND'] = " الى صديق.";
+	$lang['_YOURNAME'] = "اسمك:";
+	$lang['_YOUREMAIL'] = "بريدك الالكترونى:";
+	$lang['_SENDFR'] = "ارسال";
+
+	$lang['_ADDANNONCE'] = "أضف إعلانا جديدا داخل التصنيف الحالي";
+	$lang['_YES'] = "نعم";
+	$lang['_NO'] = "لا";
+	$lang['_BACK'] = "الى الخلف";
+	$lang['_CANCEL'] = "إلغاء";
+	$lang['_CONFDEL'] = "تأكيد الحذف";
+	$lang['_SURDELANN'] = "؟الاهتمام : هل أنت متأكد من أنك تريد حذف هذا الإعلان";
+	$lang['_ADSADDED'] = "هذا الاعلان تم اضافته";
+	$lang['_FROM'] = "مقدم بواسطة ";
+	$lang['_CLASSIFIED_AD'] = "تصنيف الاعلان:";
+	$lang['_SEARCH'] = "بحث";
+	$lang['_VIEWALLADS'] = "استعراض كل الاعلانات";
+
+	$lang['_YOURPHONE'] = "تليفونك:";
+	$lang['_YOURMESSAGE'] = "رسالتك:";
+	$lang['_VALIDMESS'] = "الرساله المطلوبه.";
+	$lang['_MESSEND'] = "رسالتك تم ارسالها...";
+	$lang['_CLASSIFIED'] = "تصنيف الاعلان ";
+	$lang['_VIEW_MY_ADS'] = "اعرض كل الإعلانات من ";	
+
+	$lang['_DELETE'] = "حذف";
+	$lang['_EDITDESC'] = "تحرير الوصف";
+	$lang['_DELETED'] = "تم حذف الصورة";
+	$lang['_EDIT'] = "تحرير";
+	$lang['_SUBMITTER'] = "الاسم :";
+	$lang['_ADD_LISTING'] = "أضف القائمة";
+	$lang['_SUBMIT'] = "ارسال";
+	
+	$lang['_ACTIVE'] = "نشط";
+	$lang['_INACTIVE'] = "غير نشط";
+
+	$lang['_FRIENDTITLE'] = "أرسل هذا الإعلان لصديق";
+	$lang['_ADDETAIL'] = "تفاصيل الاعلان:";
+	$lang['_SENDTOF'] = "إرسال هذه الصفحة إلى صديق";
+	$lang['_REPLY'] = "الرد على الاعلان";
+
+//// from 1.2.0-d
+
+	$lang['_HOW_LONG'] = "الى متى تريد العرض:";
+	$lang['_DAY'] = " أيام";
+	$lang['_CONTACTBY'] = "اظهر بريدك الالكترونى:";
+	$lang['_YES_CONTACT'] = "نعم";
+	$lang['_NO_CONTACT'] = "لا";
+	$lang['_TERM'] = "اتفاقية الاستخدام:";
+	
+	$lang['_FRIENDNAME'] = "اسم صديقك:";
+	$lang['_FRIENDMAIL'] = "البريد الالكترونى لصديقك:";
+
+	$lang['_FRIENDBTN1'] = "انظر الى الصورة ومزيد من التفاصيل, انقر على الوصلة التالية أو نسخ ولصق العنوان فى المتصفح الخاص بكم.<br><br>";
+	$lang['_FRIENDBTN2'] = "<BR><hr>الإعلانات المبوبة مجانا مع الصور.";
+
+//// from 1.2.0-g
+	$lang['_NEWADDURATION'] = "إنتهاء مدة الاعلان";
+	$lang['_NEWADDEFAULT'] = "مدة إعلان جديدة (أيام):";
+	$lang['_SENDREMIDE'] = "إرسال رسائل البريد الإلكتروني قبل انتهاء المده (يوم):";
+	$lang['_NOTMESSAGE'] = "رسائل الاشعار:";
+	$lang['_NOTMESSAGESUBJECT'] = "موضوع رسالة التذكير بانتهاء الصلاحية";
+	$lang['_NOTMESSAGEBODY'] = "نص رسالة التذكير بانتهاء الصلاحية:";
+
+	$lang['_ADMAXLIMIT'] = "حد طول النص للاعلان الموحد:";
+	$lang['_ADMAXLIMITTXT'] = "الحد الأقصى للسماح للشخصيات في تصنيف هيئة إعلانية.";
+
+	$lang['_AUTHORSEARCH'] = "المؤلف";	
+	$lang['_INVALIDTERM'] = "يرجى التحقق من اتفاقية الاستخدام";
+
+//// from 1.2.0-h
+	$lang['_DATEFORMAT'] = "شكل التاريخ:";
+	$lang['_ALLOWRSS'] = "السماح بتغذية أر إس إس";
+	$lang['_COMFCODE'] = "اظهر رمز التأكيد";
+	$lang['_NOPOSTS'] = "ادخال عدد الاعلانات الحديثة:";
+
+//// from 1.2.1
+	$lang['_NEWPOST'] = "إخطار الموافقة /الاستعراض على الاعلانات الجديده!";
+	$lang['_NOTIFYADMIN'] = "أخطرت : مدير البرنامج";
+	$lang['_NOTNOTIFY'] = "لم يخطر";
+	$lang['_POSTON'] = "نشر بتاريخ";
+	$lang['_SAVEADINFO'] = "تم حفظ السرية الاعلانية.";
+	$lang['_UPLOAD'] = "تحميل";
+	$lang['_DELETESURE'] = "هل أنت متأكد من أنك تريد حذف هذا؟";
+
+// from 1.3.0-c
+	$lang['_TOCONTACTBY'] = "للاتصال عن طريق البريد الإلكتروني الرجاء استخدام نموذج الاتصال على موقعنا بالنقر على البريد الإلكتروني في ربط الإعلانية ، يمكنك عرض الإعلان على العنوان التالي على الشبكة العالمية:";
+	$lang['_ADLINKINFO'] = "وهذا الإعلان هو من باب الإعلانات المبوبة على الموقع ";
+	$lang['_ADDIMAGE'] = "أضف صورة";
+	$lang['_DELIMAGE'] = "حذف صورة";
+
+
+
+	//$lang['_RETURNANN'] = "Return classified ads listing";
+	//$lang['_DU'] = "added on";   
+	//$lang['_SENDBY'] = "Added by :";
+	//$lang['_NUMANNN'] = "Classified Artikel No. :";	
+	//$lang['_MESSAGE'] = "thinks this Classified Artikel might interest you and has sent you this message.";
+	//$lang['_INTERESS'] = "Other Classified Artikels can be seen at ";
+	//$lang['_BYMAIL'] = "Email :";
+	//$lang['_DISPO'] = "expires on";
+	//$lang['_NOMAIL'] = "We do not give out users email addresses, to contact them by e-mail please use the contact form on our site by clicking on the e-mail link in the ad, you can view the ad at the following web address. ";
+
+?>
