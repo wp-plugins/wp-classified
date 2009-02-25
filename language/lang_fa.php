6aa52a565c0a3c000cb42a351ab5c4280469958d
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-25 08:21:53 +0000

    initial
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/tags/1.3.0-h@97520 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ language/lang_fa.php
@@ -0,0 +1,174 @@
+<?php
+// Translated to Farsi by Mohammad Forgani
+// Web site:  http://farsi-news.com
+// Email: info@farsi-news.com
+
+    $lang=array();
+	//
+	$lang['_OR'] = "و";
+	$lang['_CAT'] = "طبقه :";
+	$lang['_CHARMAX'] = "حداکثر حروف";
+	$lang['_SURNAME'] = "أسم :";
+	$lang['_VALIDATE'] = "صحت";
+	$lang['_SELECTYPE'] = "انتخاب تيپ";
+	$lang['_SELECTCAT'] = "انتخاب طبقه";
+	$lang['_SELECTCATOTHER'] = "طبقه ديگر";
+	$lang['_RETURN'] = "رجوع";
+		
+	$lang['_INVALIDEMAIL'] = "!درج ايميل ضروري است";
+	$lang['_INVALIDEMAIL2'] = "!آدرس پست الكترونيك اشتباه است";
+	$lang['_INVALIDNAME'] = "!نام كاربري حداقل بايد 2 حرف داشته باشد";
+	$lang['_INVALIDSUBJECT'] = "!درج موضوع ضروري است ";
+	$lang['_INVALIDCOMMENT'] = "درج متن ضروري است";
+	$lang['_INVALIDMSG'] = "Please correct these errors to save the listing.";
+	$lang['_INVALIDMSG2'] = "تصوير بايد";
+	$lang['_INVALIDMSG3'] = " pixels or less.";
+	$lang['_INVALIDMSG4'] =  "تصوير بايد کوچکتر از";
+	$lang['_INVALIDCONFIRM'] = "کد مطابقت ندارد";
+	$lang['_INVALIDIMG'] = "اندازه عکس معتبر نيست";
+	$lang['_YIMG'] = "عکس شما";
+
+	$lang['_OPTIONAL'] = "دلخواه";
+	$lang['_REQUIRED'] = "ضروري";
+	$lang['_SAVEAD'] = "ذخيره"; 
+
+	$lang['_DESC'] = "توضیح:";
+	$lang['_PRINT'] = "چاپ اين اگهي";
+	$lang['_EMAIL'] = "پست الکترونیک:";
+	$lang['_TEL'] = "تلفن:";
+	$lang['_WEB'] = "وب سايت:";
+	$lang['_CONFIRM'] = "رمز:";
+	$lang['_PIC'] = "تصویر:";
+	$lang['_TITLE'] = "عنوان:"; // subject
+	$lang['_SUBJECT'] = "موضوع"; //posting name
+	$lang['_AUTHOR'] = "نام/ شركت:";
+	$lang['_TEL'] = "تلفن:";
+	$lang['_DATE'] = "تاریخ";
+	$lang['_LIST'] = "فهرست";
+	$lang['_VIEWS'] = "نمایش";
+	$lang['_ADS'] = "إعلانات";
+	$lang['_LAST'] = "آخرین آگهی";
+	$lang['_UPDATE'] = "به‌روز رساني شد";
+	$lang['_SEND'] = "پيغام شما ارسال گرديد متشکريم";
+	$lang['_SENDNOTFAIL'] = "ارسال پيغام دچار مشکل شد";
+	$lang['_SENDNOT'] = "پيغام ارسال گرديد";
+	$lang['_ANNSEND'] = "آگهي انتخاب شده فرستاده شد";
+	$lang['_ANNDEL'] = "آگهي حذف شد";
+	
+	$lang['_ANNMOD2'] = "آگهی تغییر داده شده";
+	$lang['_THANKS'] = "با تشكر";
+	$lang['_MUSTLOGIN'] = "براي درج اگهي احتياج به ثبت نام داريد";
+	$lang['_GUEST'] = "انك لم تقم بتسجيل الدخول كضيف, إضغط";
+	$lang['_SENDEMAIL'] = "ارسال الى برديك الالكترونى";
+	$lang['_SENDERR'] = "ارسال پيغام دچار مشکل شد";
+	$lang['_EXTRANN'] = "This advertisement is from the classified ads section on the website ";
+	$lang['_FRIENDSEND'] = "<b>Send this advertisement to a friend</b><br>You can send ad No ";
+	$lang['_SORRY'] = "Sorry, it seems that you do not have permission to perform the requested action.";
+	$lang['_TOAFRIEND'] = "به يک دوست";
+	$lang['_YOURNAME'] = "نام شما:";
+	$lang['_YOUREMAIL'] = "پست الکترونيک :";
+	$lang['_SENDFR'] = "ارسال";
+
+	$lang['_ADDANNONCE'] = "درچ يک اگهي چديد  در اين فهرست";
+	$lang['_YES'] = "بله";
+	$lang['_NO'] = "نه";
+	$lang['_BACK'] = "بازگشت";
+	$lang['_CANCEL'] = "انصراف";
+	$lang['_CONFDEL'] = "?آیا مطمئنید که میخواهید حذف شود";
+	$lang['_SURDELANN'] = "ATTENTION : Are you sure you want to delete this advertisement?";
+	$lang['_ADSADDED'] = "این آگهی فرستاده شده است";
+	$lang['_FROM'] = "ارسال شده ازطرف";
+	$lang['_CLASSIFIED_AD'] = "تصنيف الاعلان:";
+	$lang['_SEARCH'] = "جستجو";
+	$lang['_VIEWALLADS'] = "نمايش اگهي ها";
+
+	$lang['_YOURPHONE'] = "تلفن:";
+	$lang['_YOURMESSAGE'] = "متن پیام:";
+	$lang['_VALIDMESS'] = "الرساله المطلوبه.";
+	$lang['_MESSEND'] = "پيغام شما ارسال شد...";
+	$lang['_CLASSIFIED'] = "دسته بندها";
+	$lang['_VIEW_MY_ADS'] = "نمايش اگهي ها";	
+
+	$lang['_DELETE'] = "حذف";
+	$lang['_EDITDESC'] = "ویرایش توضیحات";
+	$lang['_DELETED'] = "حذف تصویر انچام شد";
+	$lang['_EDIT'] = "ویرایش";
+	$lang['_SUBMITTER'] = "نویسنده:";
+	$lang['_ADD_LISTING'] = "اضافه به فهرست";
+	$lang['_SUBMIT'] = "ارسال";
+	
+	$lang['_ACTIVE'] = "فعال";
+	$lang['_INACTIVE'] = "غیر فعال";
+
+	$lang['_FRIENDTITLE'] = "Send this advertisement to a friend";
+	$lang['_ADDETAIL'] = "شرح اگهي:";
+	$lang['_SENDTOF'] = "ارسال اين اگهي به يک دوست";
+	$lang['_REPLY'] = "پاسخ به پست";
+
+//// from 1.2.0-d
+
+	$lang['_HOW_LONG'] = "مدت نمايش:";
+	$lang['_DAY'] = " روز";
+	$lang['_CONTACTBY'] = "?ايميل نمايش داده شود:";
+	$lang['_YES_CONTACT'] = "بله";
+	$lang['_NO_CONTACT'] = "نه";
+	$lang['_TERM'] = "اتفاقية الاستخدام:";
+	
+	$lang['_FRIENDNAME'] = "نام دوست شما:";
+	$lang['_FRIENDMAIL'] = "ایمیل دوست شما:";
+
+	$lang['_FRIENDBTN1'] = "To see view photo and more details, click on the following link or copy and paste it to the address window in you browser.<br><br>";
+	$lang['_FRIENDBTN2'] = "<BR><hr>Free classified ads with photos.";
+
+//// from 1.2.0-g
+	$lang['_NEWADDURATION'] = "زمان انقضا را مشخص کنید";
+	$lang['_NEWADDEFAULT'] = "New ad default duration (days):";
+	$lang['_SENDREMIDE'] = "Send reminder emails before the expiration (days):";
+	$lang['_NOTMESSAGE'] = "پیغام:";
+	$lang['_NOTMESSAGESUBJECT'] = "Expiration reminder email subject";
+	$lang['_NOTMESSAGEBODY'] = "Expiration reminder email body:";
+
+	$lang['_ADMAXLIMIT'] = "Body length limit for standard ads:";
+	$lang['_ADMAXLIMITTXT'] = "Maximum characters to allow in classified ad body.";
+
+	$lang['_AUTHORSEARCH'] = "نویسنده";	
+	$lang['_INVALIDTERM'] = "Please check the Terms of Use Agreement";
+
+//// from 1.2.0-h
+	$lang['_DATEFORMAT'] = "شكل تاريخ:";
+	$lang['_ALLOWRSS'] = "نمايش اراس اس:";
+	$lang['_COMFCODE'] = "نمايش کد اتفاقي:";
+	$lang['_NOPOSTS'] = "تعداد نمايش اگهي در فيد:";
+
+//// from 1.2.1
+	$lang['_NEWPOST'] = "Notification of new ad approval/review!";
+	$lang['_NOTIFYADMIN'] = "Notified: Administrator";
+	$lang['_NOTNOTIFY'] = "Not Notified";
+	$lang['_POSTON'] = "تاريخ پست";
+	$lang['_SAVEADINFO'] = "آگهی شما ذخیره شد";
+	$lang['_UPLOAD'] = "آپلود";
+	$lang['_DELETESURE'] = "?مطمئن هستید که عکس پاک شود";
+
+// from 1.3.0-c
+	$lang['_TOCONTACTBY'] = "To contact by e-mail please use the contact form on our site by clicking on the e-mail link in the ad, you can view the ad at the following web address:";
+	$lang['_ADLINKINFO'] = "This advertisement is from the classified ads section on the website ";
+	$lang['_ADDIMAGE'] = "اضافه عکس";
+	$lang['_DELIMAGE'] = "حذف عکس";
+
+	$lang['_MAIN'] = "ابتدا";
+
+// from 1.3.1-b
+	$lang['_APPROVE'] = "Thank You for using our Classified Ads.<br>Your classified ad has been sucessfully submitted for review!<br>Feel free to submit another ad.";
+	$lang['_APPROVEREPLY'] = "This ad was already submitted. We will check it as soon as possible.";
+
+	//$lang['_RETURNANN'] = "Return classified ads listing";
+	//$lang['_DU'] = "added on";   
+	//$lang['_SENDBY'] = "Added by :";
+	//$lang['_NUMANNN'] = "Classified Artikel No. :";	
+	//$lang['_MESSAGE'] = "thinks this Classified Artikel might interest you and has sent you this message.";
+	//$lang['_INTERESS'] = "Other Classified Artikels can be seen at ";
+	//$lang['_BYMAIL'] = "Email :";
+	//$lang['_DISPO'] = "expires on";
+	//$lang['_NOMAIL'] = "We do not give out users email addresses, to contact them by e-mail please use the contact form on our site by clicking on the e-mail link in the ad, you can view the ad at the following web address. ";
+
+?>
