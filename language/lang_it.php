07af0991c9faad364bb6cde02187f813afcf02cd
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-13 07:40:42 +0000

    initial
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/tags/1.3.0-g@172878 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ language/lang_it.php
@@ -0,0 +1,189 @@
+
+<?php
+
+// Translated to  italian by http://www.nuotoacquelibere.com
+
+    $lang=array();
+        //
+        $lang['_OR'] = "o";
+        $lang['_CAT'] = "Categoria :";
+        $lang['_CHARMAX'] = "Massimo numero di caratteri";
+        $lang['_SURNAME'] = "Nome :";
+        $lang['_VALIDATE'] = "Validazione";
+        $lang['_SELECTYPE'] = "Seleziona un tipo di annuncio";
+        $lang['_SELECTCAT'] = "Seleziona una categoria";
+        $lang['_SELECTCATOTHER'] = "Altra categoria";
+        $lang['_RETURN'] = "Ritorna";
+                
+        $lang['_INVALIDEMAIL'] = "L'e-mail è necessaria.";
+        $lang['_INVALIDEMAIL2'] = "Inserire una e-mail valida!";
+        $lang['_INVALIDNAME'] = "Inserire un nome!";
+        $lang['_INVALIDSUBJECT'] = "Inserire un soggetto!";
+        $lang['_INVALIDCOMMENT'] = "La descrizione è necessaria.";
+        $lang['_INVALIDMSG'] = "Correggere i seguenti errori per salvare.";
+        $lang['_INVALIDMSG2'] = "L'immagine deve essere ";
+        $lang['_INVALIDMSG3'] = " pixel o meno. ";
+        $lang['_INVALIDMSG4'] =  "L'immagine deve essere meno di ";
+        $lang['_INVALIDCONFIRM'] = "il codice di conferma è errato.";
+        $lang['_INVALIDIMG'] = "Dimensioni immagine non valida.";
+        $lang['_YIMG'] = "La tua immagine era";
+
+        $lang['_OPTIONAL'] = " opzionale. ";
+        $lang['_REQUIRED'] = " richiesto. ";
+        $lang['_SAVEAD'] = "Salva Annuncio";
+
+        $lang['_DESC'] = "Descrizione:";
+        $lang['_PRINT'] = "Stampa Annuncio";
+        $lang['_EMAIL'] = "E-mail:";
+        $lang['_TEL'] = "Telefono:";
+        $lang['_WEB'] = "Sito:";
+        $lang['_CONFIRM'] = "Codice di conferma:";
+        $lang['_PIC'] = "Immagine:";
+        $lang['_TITLE'] = "Titolo:"; // subject
+        $lang['_SUBJECT'] = "Soggetto"; //posting name
+        $lang['_AUTHOR'] = "Nome:";
+        $lang['_TEL'] = "Telefono/Cellulare:";
+        $lang['_DATE'] = "Data";
+        $lang['_LIST'] = "Lista";
+        $lang['_VIEWS'] = "Visite";
+        $lang['_ADS'] = "Annunci";
+        $lang['_LAST'] = "Ultimo annuncio";
+        $lang['_UPDATE'] = "Aggiornamento completato.";
+        $lang['_SEND'] = "La tua e-mail è stat inviata. Grazie.";
+        $lang['_SENDNOTFAIL'] = "La notifica via e-mail è fallita.";
+        $lang['_SENDNOT'] = "La notifica via e-mail è stat inviata.";
+        $lang['_ANNSEND'] = "L'annuncio selezionato è stato inviato.";
+        $lang['_ANNDEL'] = "L'annuncio è stato cancellato.";
+        
+        $lang['_ANNMOD2'] = "L'annuncio è stato modificato.";
+        $lang['_THANKS'] = "Grazie per aver usato la nostra bacheca Annunci.";
+        $lang['_MUSTLOGIN'] = "Loggarsi per inserire un annuncio.";
+        $lang['_GUEST'] = "Non sei loggato e posti come Ospite, clicca";
+        $lang['_SENDEMAIL'] = "Invia E-mail";
+        $lang['_SENDERR'] = "Problema di invio e-mail. Controllare il form.";
+        $lang['_EXTRANN'] = "Questo annuncio fa parte della bacheca annunci del sito ";
+        $lang['_FRIENDSEND'] = "<b>Invia ad un amico</b><br>Puoi inviare ";
+        $lang['_SORRY'] = "Non si hanno i permessi necessari.";
+        $lang['_TOAFRIEND'] = " ad un amico.";
+        $lang['_YOURNAME'] = "Tuo nome:";
+        $lang['_YOUREMAIL'] = "Tua e-mail:";
+        $lang['_SENDFR'] = "Invia";
+
+        $lang['_ADDANNONCE'] = "Aggiungi un annuncio in questa categoria";
+        $lang['_YES'] = "Si";
+        $lang['_NO'] = "No";
+        $lang['_BACK'] = "Indietro";
+        $lang['_CANCEL'] = "Cancella";
+        $lang['_CONFDEL'] = "Conferma di cancellazione";
+        $lang['_SURDELANN'] = "ATTENZIONE : vuoi cancellare questo annuncio?";
+        $lang['_ADSADDED'] = "L'annuncio è stato aggiunto.";
+        $lang['_FROM'] = "Scritto da ";
+        $lang['_CLASSIFIED_AD'] = "Annuncio:";
+        $lang['_SEARCH'] = "Cerca";
+        $lang['_VIEWALLADS'] = "Tutti gli annunci";
+
+        $lang['_YOURPHONE'] = "Tuo telefono:";
+        $lang['_YOURMESSAGE'] = "Tuo messaggio:";
+        $lang['_VALIDMESS'] = "Il messaggio è necessario.";
+        $lang['_MESSEND'] = "il tuo messaggio è stato inviato...";
+        $lang['_CLASSIFIED'] = "Annnuncio ";
+        $lang['_VIEW_MY_ADS'] = "Tutti gli annunci di ";        
+
+        $lang['_DELETE'] = "cancella";
+        $lang['_EDITDESC'] = "Modifica la descrizione";
+        $lang['_DELETED'] = "Immagine cancellata.";
+        $lang['_EDIT'] = "modifica";
+        $lang['_SUBMITTER'] = "Nome :";
+        $lang['_ADD_LISTING'] = "Aggiungi un annuncio";
+        $lang['_SUBMIT'] = "Invia";
+        
+        $lang['_ACTIVE'] = "Attivo";
+        $lang['_INACTIVE'] = "inattivo";
+
+        $lang['_FRIENDTITLE'] = "Invia ad un amico questo annuncio.";
+        $lang['_ADDETAIL'] = "Dettagli annuncio:";
+        $lang['_SENDTOF'] = "Invia ad un amico";
+        $lang['_REPLY'] = "Rispondi";
+
+//// from 1.2.0-d
+
+        $lang['_HOW_LONG'] = "Per quanto tempo lo vuoi lasciar pubblicato?";
+        $lang['_DAY'] = " giorni";
+        $lang['_CONTACTBY'] = "Mostra la tua e-mail:";
+        $lang['_YES_CONTACT'] = "Si";
+        $lang['_NO_CONTACT'] = "No";
+        $lang['_TERM'] = "Accetto le condizioni di utilizzo:";
+        
+        $lang['_FRIENDNAME'] = "Nome del tuo amico:";
+        $lang['_FRIENDMAIL'] = "Email del tuo amico:";
+
+        $lang['_FRIENDBTN1'] = "Per vedere i dettagli di questo annuncio copia nel browser o clicca sul seguente link.<br><br>";
+        $lang['_FRIENDBTN2'] = "<BR><hr>Annunci gratuiti con foto.";
+
+//// from 1.2.0-g
+        $lang['_NEWADDURATION'] = "Durata Annuncio";
+        $lang['_NEWADDEFAULT'] = "Nuova scadenza (giorni):";
+        $lang['_SENDREMIDE'] = "Invia un messaggio alcuni giorni prima della scadenza (giorni):";
+        $lang['_NOTMESSAGE'] = "Messaggio di notifica:";
+        $lang['_NOTMESSAGESUBJECT'] = "Oggetto messaggio di notifica";
+        $lang['_NOTMESSAGEBODY'] = "Messaggio messaggio di notifica:";
+
+        $lang['_ADMAXLIMIT'] = "Lunghezza annuncio:";
+        $lang['_ADMAXLIMITTXT'] = "Massimo numero di caratteri.";
+
+        $lang['_AUTHORSEARCH'] = "Autore";      
+        $lang['_INVALIDTERM'] = "Leggi le condizioni di utilizzo";
+
+//// from 1.2.0-h
+        $lang['_DATEFORMAT'] = "Formato data:";
+        $lang['_ALLOWRSS'] = "Visualizza RSS:";
+        $lang['_COMFCODE'] = "Mostra il codice di conferma:";
+        $lang['_NOPOSTS'] = "Numero di annunci nell'RSS:";
+
+//// from 1.2.1
+        $lang['_NEWPOST'] = "Notifica nuovo annuncio!";
+        $lang['_NOTIFYADMIN'] = "Notificato all'Amministratore";
+        $lang['_NOTNOTIFY'] = "non notificato";
+        $lang['_POSTON'] = "Pubblicato il";
+        $lang['_SAVEADINFO'] = "Annuncio salvato.";
+        $lang['_UPLOAD'] = "Carica";
+        $lang['_DELETESURE'] = "Sei sicuro di volere cancellare questo?";
+
+// from 1.3.0-c
+        $lang['_TOCONTACTBY'] = "Contattaci dal form nell'annuncio che trovi
+a qeusto indirizzo:";
+        $lang['_ADLINKINFO'] = "Questo annuncio è della bacheca di annunci del sito ";
+        $lang['_ADDIMAGE'] = "Aggiungi immagine";
+        $lang['_DELIMAGE'] = "Cancella immagine";
+
+// from 1.3.g
+
+        $lang['_INVALIDURL'] = "Inserire un indirizzo valido!";
+        $lang['_INVALIDPHONE'] = "Inserire un numero di telefono valido!";
+
+// from 1.3.1-b
+        $lang['_INVALIDTITLE'] = "Il titolo deve contenere massimo 30
+caratteri (lettere e numeri).";
+        $lang['_MAIN'] = "Bacheca";
+
+// from 1.3.1-b
+        $lang['_APPROVE'] = "Grazie per aver usato la nostra bacheca
+annunci.<br>Il tuo annuncio è in fase di revisione!<br>Se vuoi puoi
+inserire un altro annuncio.";
+        $lang['_APPROVEREPLY'] = "Questo annuncio è già stato inviato, lo
+revisioneremo appena possibile.";
+
+        //$lang['_RETURNANN'] = "Lista Annuncio";
+        //$lang['_DU'] = "aggiunto il";
+        //$lang['_SENDBY'] = "Aggiunto da :";
+        //$lang['_NUMANNN'] = "Annuncio Num. :";        
+        //$lang['_MESSAGE'] = "pensa che ti possa interessare è ti ha inviato
+il link.";
+        //$lang['_INTERESS'] = "Altri annunci possono essere visionati qui ";
+        //$lang['_BYMAIL'] = "E-mail :";
+        //$lang['_DISPO'] = "scadenza";
+        //$lang['_NOMAIL'] = "Gli indirizzi e-mail non sono in chiaro, per
+contattre gli inserzionisti utilizza il form nell'annuncio che puoi
+trovare qui: ";
+
+?>
---
+++ language/lang_ro.php
@@ -0,0 +1,184 @@
+<?php
+
+/* 
+Localization by:
+        Mihai Codreanu(http://www.mihaicodreanu.com)
+*/
+
+
+    $lang=array();
+	//
+	$lang['_OR'] = "sau";
+	$lang['_CAT'] = "Categorie :";
+	$lang['_CHARMAX'] = "Numărul maxim de cacractere";
+	$lang['_SURNAME'] = "Nume :";
+	$lang['_VALIDATE'] = "Validare";
+	$lang['_SELECTYPE'] = "Alege tipul anunţului";
+	$lang['_SELECTCAT'] = "Alege categoria";
+	$lang['_SELECTCATOTHER'] = "Altă categorie";
+	$lang['_RETURN'] = "Întoarcere";
+		
+	$lang['_INVALIDEMAIL'] = "Email-ul este necesar";
+	$lang['_INVALIDEMAIL2'] = "Introduceţi o adresă de email reală";
+	$lang['_INVALIDNAME'] = "Trebuie introdus numele!";
+	$lang['_INVALIDSUBJECT'] = "Trebuie introdus titlul!";
+	$lang['_INVALIDCOMMENT'] = "Descrierea este necesară.";
+	$lang['_INVALIDMSG'] = "Vă rugăm corectaţi aceste erori pentru a salva listarea.";
+	$lang['_INVALIDMSG2'] = "Imaginile trebuie să fie de ";
+	$lang['_INVALIDMSG3'] = " pixeli sau mai puţin. ";
+	$lang['_INVALIDMSG4'] =  "Imaginea trebuie să fie mai mică de ";
+	$lang['_INVALIDCONFIRM'] = "Codul de confirmare e incorect.";
+	$lang['_INVALIDIMG'] = "Imaginea este prea mare.";
+	$lang['_YIMG'] = "Imaginea a fost ";
+
+	$lang['_OPTIONAL'] = " opţional. ";
+	$lang['_REQUIRED'] = " necesar. ";
+	$lang['_SAVEAD'] = "Salvare anunţ"; 
+
+	$lang['_DESC'] = "Descriere:";
+	$lang['_PRINT'] = "Listează acest anunţ";
+	$lang['_EMAIL'] = "Email:";
+	$lang['_TEL'] = "Telefon:";
+	$lang['_WEB'] = "WebSite:";
+	$lang['_CONFIRM'] = "Codul de confirmare:";
+	$lang['_PIC'] = "Imagine:";
+	$lang['_TITLE'] = "Titlu:"; // subject
+	$lang['_SUBJECT'] = "Subiect"; //posting name
+	$lang['_AUTHOR'] = "Numele:";
+	$lang['_TEL'] = "Mobil:";
+	$lang['_DATE'] = "Data";
+	$lang['_LIST'] = "Lista";
+	$lang['_VIEWS'] = "Vizualizări";
+	$lang['_ADS'] = "Anunţuri";
+	$lang['_LAST'] = "Ultimul anunţ";
+	$lang['_UPDATE'] = "Reîmprospătat cu succes.";
+	$lang['_SEND'] = "Emailul a fost trimis. Mulţumim.";
+	$lang['_SENDNOTFAIL'] = "Notificarea prin email a eşuat.";
+	$lang['_SENDNOT'] = "Notificarea prin email a fost trimisă.";
+	$lang['_ANNSEND'] = "Anunţul selectat a fost trimis.";
+	$lang['_ANNDEL'] = "Anunţul a fost şters.";
+	
+	$lang['_ANNMOD2'] = "Anunţul a fost modificat.";
+	$lang['_THANKS'] = "Vă mulţumim pentru că aţi apelat la anunţurile noastre.";
+	$lang['_MUSTLOGIN'] = "Trebuie să vă logaţi pentru a adăuga un anunţ.";
+	$lang['_GUEST'] = "Nu sunteţi logat şi postaţi ca invitat, click";
+	$lang['_SENDEMAIL'] = "Trimite email";
+	$lang['_SENDERR'] = "A fost o problemă cu trimitea emailului. Vă rugăm să verificaţi dacă aţi completat formularul corect.";
+	$lang['_EXTRANN'] = "Această reclamă este din secţiunea de anunţuri a siteului ";
+	$lang['_FRIENDSEND'] = "<b>Trimiteţi acest anunţ unui prieten</b><br>Puteţi trimite anunţul ";
+	$lang['_SORRY'] = "Ne pare rău, se pare că nu aveţi permisiune pentru această acţiune.";
+	$lang['_TOAFRIEND'] = " unui prieten.";
+	$lang['_YOURNAME'] = "Numele:";
+	$lang['_YOUREMAIL'] = "Emailul:";
+	$lang['_SENDFR'] = "Trimite";
+
+	$lang['_ADDANNONCE'] = "Adaugă un anunţ în această categorie";
+	$lang['_YES'] = "Da";
+	$lang['_NO'] = "Nu";
+	$lang['_BACK'] = "Înainte";
+	$lang['_CANCEL'] = "Anulare";
+	$lang['_CONFDEL'] = "Confirmă ştergerea";
+	$lang['_SURDELANN'] = "Atenţie: Sunteţi sigur că vreţi să ştergeţi acest anunţ?";
+	$lang['_ADSADDED'] = "Acest anunţ a fost adăugat";
+	$lang['_FROM'] = "Adăugat de ";
+	$lang['_CLASSIFIED_AD'] = "Anunţ:";
+	$lang['_SEARCH'] = "Caută";
+	$lang['_VIEWALLADS'] = "Vezi toate anunţurile";
+
+	$lang['_YOURPHONE'] = "Telefon:";
+	$lang['_YOURMESSAGE'] = "Mesaj:";
+	$lang['_VALIDMESS'] = "Mesajul este necesar.";
+	$lang['_MESSEND'] = "mesajul a fost trimis...";
+	$lang['_CLASSIFIED'] = "Anunţ ";
+	$lang['_VIEW_MY_ADS'] = "Vezi toate anunţurile din ";	
+
+	$lang['_DELETE'] = "Şterge";
+	$lang['_EDITDESC'] = "Modifică descrierea";
+	$lang['_DELETED'] = "Imaginea a fost ştearsă";
+	$lang['_EDIT'] = "Editează";
+	$lang['_SUBMITTER'] = "Nume :";
+	$lang['_ADD_LISTING'] = "Adaugă o listă";
+	$lang['_SUBMIT'] = "Trimite";
+	
+	$lang['_ACTIVE'] = "Activ";
+	$lang['_INACTIVE'] = "Inactiv";
+
+	$lang['_FRIENDTITLE'] = "Trimite acest anunţ unui prieten";
+	$lang['_ADDETAIL'] = "Detalii despre anunţ:";
+	$lang['_SENDTOF'] = "Trimite unui prieten";
+	$lang['_REPLY'] = "Răspunde la acest post";
+
+//// from 1.2.0-d
+
+	$lang['_HOW_LONG'] = "Cât timp vreţi să apară:";
+	$lang['_DAY'] = " zile";
+	$lang['_CONTACTBY'] = "Arată adresa de email:";
+	$lang['_YES_CONTACT'] = "Da";
+	$lang['_NO_CONTACT'] = "Nu";
+	$lang['_TERM'] = "Termeni de utilizare:";
+	
+	$lang['_FRIENDNAME'] = "Numele prietenului:";
+	$lang['_FRIENDMAIL'] = "Email-ul prietenului:";
+
+	$lang['_FRIENDBTN1'] = "Pentru a vedea imaginea şi alte detalii, daţi click pe următorul link sau copiaţi-l în bara de adresă a browserului web.<br><br>";
+	$lang['_FRIENDBTN2'] = "<BR><hr>Anunţuri gratuite cu imagine.";
+
+//// from 1.2.0-g
+	$lang['_NEWADDURATION'] = "Durata şi expirarea anunţului";
+	$lang['_NEWADDEFAULT'] = "Noua durată implicită a anuţului (zile):";
+	$lang['_SENDREMIDE'] = "Trimite email înainte de expirare (zile):";
+	$lang['_NOTMESSAGE'] = "Mesaj de notificare:";
+	$lang['_NOTMESSAGESUBJECT'] = "Subiectul notificării de expirare";
+	$lang['_NOTMESSAGEBODY'] = "Textul notificării de expirare:";
+
+	$lang['_ADMAXLIMIT'] = "Lungimea textului pentru anunţuri standard:";
+	$lang['_ADMAXLIMITTXT'] = "Numărul maxim de caractere din anunţ.";
+
+	$lang['_AUTHORSEARCH'] = "Autor";	
+	$lang['_INVALIDTERM'] = "Vă rugăm să citiţi Termenii de utilizare";
+
+//// from 1.2.0-h
+	$lang['_DATEFORMAT'] = "Formatul dăţii:";
+	$lang['_ALLOWRSS'] = "Permite flux RSS:";
+	$lang['_COMFCODE'] = "Arată codul de confirmare:";
+	$lang['_NOPOSTS'] = "Numărul de posturi recente:";
+
+//// from 1.2.1
+	$lang['_NEWPOST'] = "Notificare pentru aprobarea de anunţuri noi!";
+	$lang['_NOTIFYADMIN'] = "Notificat: Administrator";
+	$lang['_NOTNOTIFY'] = "Nu a fost notificat";
+	$lang['_POSTON'] = "Postat pe";
+	$lang['_SAVEADINFO'] = "Anunţul a fost salvat.";
+	$lang['_UPLOAD'] = "Uploadare";
+	$lang['_DELETESURE'] = "Sunteţi sigur că vreţi să ştergeţi?";
+
+// from 1.3.0-c
+	$lang['_TOCONTACTBY'] = "Pentru a contacta prin email vă rugăm să folosiţi formularul de pe site făcând click pe linkul de email din anunţ, puteţi vizualiza anunţul la următoarea adresă:";
+	$lang['_ADLINKINFO'] = "Această publicitate este din secţiunea de anunţuri a sitului nostru ";
+	$lang['_ADDIMAGE'] = "Adaugă imagini";
+	$lang['_DELIMAGE'] = "Şterge imagini";
+
+// from 1.3.g
+
+	$lang['_INVALIDURL'] = "Vă rugăm să introduceţi o adresă reală!";
+	$lang['_INVALIDPHONE'] = "Vă rugăm să introduceţi un număr de telefon real!";
+
+// from 1.3.1
+	$lang['_INVALIDTITLE'] = "Titlul trebuie să conţină numai litere şi cifre şi nu trebuie să depăşească 30 de caractere.";
+	$lang['_MAIN'] = "Principal";
+
+// from 1.3.1-b
+	$lang['_APPROVE'] = "Thank You for using our Classified Ads.<br>Your classified ad has been sucessfully submitted for review!<br>Feel free to submit another ad.";
+	$lang['_APPROVEREPLY'] = "This ad was already submitted. We will check it as soon as possible.";
+
+	//$lang['_RETURNANN'] = "Return classified ads listing";
+	//$lang['_DU'] = "added on";   
+	//$lang['_SENDBY'] = "Added by :";
+	//$lang['_NUMANNN'] = "Classified Artikel No. :";	
+	//$lang['_MESSAGE'] = "thinks this Classified Artikel might interest you and has sent you this message.";
+	//$lang['_INTERESS'] = "Other Classified Artikels can be seen at ";
+	//$lang['_BYMAIL'] = "Email :";
+	//$lang['_DISPO'] = "expires on";
+	//$lang['_NOMAIL'] = "We do not give out users email addresses, to contact them by e-mail please use the contact form on our site by clicking on the e-mail link in the ad, you can view the ad at the following web address. ";
+
+?>
