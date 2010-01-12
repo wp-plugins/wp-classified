01e568e3510a688ecd563e22f84879b4702f8869
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-12 12:57:58 +0000

    initial
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/trunk@193146 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/printAd_tpl.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+* sendAd_tpl template wordpress plugin
+* This file is part of wp-classified
+* @author Mohammad Forgani 2008
+* Author Website : http://www.forgani.com
+* @version 1.2.1
+* 
+*/
+
+?>
+
+<html>
+<head>
+<title><?php echo $wpcSettings['wpClassified_slug']; ?></title>
+</head>
+<body bgcolor="#FFFFFF" text="#000000">
+
+<H2><?php echo $subject; ?></H2><BR />
+
+<?php
+	if (isset($error)){echo '<p class="error">' .$error. '</p>';}
+?>
+
+<div class="wpc_container">
+	<table border=0 width=100% cellpadding=15 cellspacing=1 bgcolor="#FFFFFF">
+		<tr><td>
+			<br /><br /><table width=99% border=0><tr>
+			<td><?php echo $lang['_CLASSIFIED_AD']; ?> (No. <?php echo  $aid; ?>)<br /><?php echo $lang['_FROM']; ?><br /><br />
+			<b><?php echo $lang['_TITLE']; ?></b> <i><?php echo $subject; ?></i><br />
+			<?php
+			foreach($array as $f) {
+				if ($f !=''){
+					include (dirname(__FILE__).'/js/viewer.js.php');
+					echo "<div class=\"show_ad_img12\"><a href=\"".get_bloginfo('wpurl')."/wp-content/plugins/wp-classified/images/" . $f . "\"><img src=\"".get_bloginfo('wpurl')."/wp-content/plugins/wp-classified/images/" . $f . "\" style=\"width: 120px; height: 100px\"></a><br>" .$f . "</div>";
+				} 
+			} 
+			echo "<p class=\"justify\"><b>".$lang['_DESC']."</b><br /><br />".$message."</p>";
+			if ($phone) {
+				echo "<br /><b>".$lang['_TEL']."</b>" . $phone . "<br />";
+			}
+			if ($web) {
+				echo"<b>".$lang['_WEB']."</b> ". $web ;
+			}
+			?>
+			<hr /><p>&nbsp;</p>
+			<?php echo $lang['_TOCONTACTBY'];?>
+			<?php
+			$url = get_bloginfo('wpurl') . "/?page_id=" . $pageinfo["ID"]. "&_action=va&asid=" . $post->ads_subjects_id;
+			echo '<br /><i>' . $url . '</li>';
+			echo "<br /><br />".$lang['_ADSADDED']. " " . "<nobr>" . @date($wpcSettings['date_format'], $post->date) ."</nobr>";
+			?>
+			<br /><?php echo $lang['_ADLINKINFO'];?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?>.</a>
+			</td>
+		</tr>
+		<tr><td class="wpc_label_right"></td><td>
+			<p><input type="button" value="Print" onClick="window.print(); parent.fb.end(true); return false;" /></p>
+		</td></tr>
+	</table>		
+</diV>
+
+
+</body>
+</html>
+
