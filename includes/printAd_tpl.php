0947fee9bba7b7152378d3c79ca3107148ee7027
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-15 09:04:03 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/trunk@194157 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/printAd_tpl.php
+++ includes/printAd_tpl.php
@@ -29,13 +29,6 @@
 			<td><?php echo $lang['_CLASSIFIED_AD']; ?> (No. <?php echo  $aid; ?>)<br /><?php echo $lang['_FROM']; ?><br /><br />
 			<b><?php echo $lang['_TITLE']; ?></b> <i><?php echo $subject; ?></i></td></tr>
 			<?php
-			foreach($array as $f) {
-				if ($f !=''){
-					echo "<tr><td>";
-					echo "<div class=\"show_ad_img12\"><img src=\"".get_bloginfo('wpurl')."/wp-content/plugins/wp-classified/images/" . $f . "\" style=\"width: 120px; height: 100px\"></a><br>" .$f . "</div>";
-					echo "</td></tr>";
-				} 
-			} 
 			echo "<tr><td>";
 			echo "<p class=\"justify\"><b>".$lang['_DESC']."</b><br /><br />".$message."</p>";
 			if ($phone) {
@@ -45,17 +38,28 @@
 				echo"<b>".$lang['_WEB']."</b> ". $web ;
 			}
 			?>
-			<hr /><p>&nbsp;</p>
-			<?php echo $lang['_TOCONTACTBY'];?>
+			</td></tr>
+			<tr><td align="center">
+			<?php
+			foreach($array as $f) {
+				?>
+				
+				<img valign=absmiddle src="<?php echo get_bloginfo('wpurl') ?>/wp-content/plugins/wp-classified/images/<?php echo $f; ?>" class="imgMiddle"  width="120" height="100">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<?php
+			}
+			?>
+			</td></tr>
+			<tr><td class="wpc_label_right">
+			<hr />
+			<p>&nbsp;</p>
 			<?php
 			$url = get_bloginfo('wpurl') . "/?page_id=" . $pageinfo["ID"]. "&_action=va&asid=" . $post->ads_subjects_id;
 			echo '<br /><i>' . $url . '</li>';
 			echo "<br /><br />".$lang['_ADSADDED']. " " . "<nobr>" . @date($wpcSettings['date_format'], $post->date) ."</nobr>";
 			?>
-			<br /><?php echo $lang['_ADLINKINFO'];?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?>.</a>
-			</td>
+			<br /><?php echo $lang['_ADLINKINFO'];?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?>.</a></td>
 		</tr>
-		<tr><td class="wpc_label_right"></td><td>
+		<tr><td class="wpc_label_right">
 			<p><input type="button" value="Print" onClick="window.print(); parent.fb.end(true); return false;" /></p>
 		</td></tr>
 	</table>		
