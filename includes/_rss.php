8ab39a4b7db057cb48d32bb95bf5eb024572e046
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-10 15:49:09 +0000

    TODO fixed for 3.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/trunk@358330 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/_rss.php
+++ includes/_rss.php
@@ -5,24 +5,28 @@ RSS Feeds
 * Author Website : http://www.forgani.com
 */
 
+/*
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+*/
+
 if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { 
 	die('You are not allowed to call this page directly.'); 
 }
 
 
-$filename = $wpClassified->cache_dir . 'wpclassified.xml';
-
 $wpcSettings = get_option('wpClassified_data');
 $limit=$wpcSettings['rss_feed_num'];
 if(!isset($limit)) $limit=20;
+
 $pageinfo = $wpClassified->get_pageinfo();
 
 # Get Data
-$sql = "SELECT {$table_prefix}wpClassified_lists.lists_id,{$table_prefix}wpClassified_lists.name, {$table_prefix}wpClassified_ads.subject, {$table_prefix}wpClassified_ads.status, {$table_prefix}wpClassified_ads.image_file,  {$table_prefix}wpClassified_ads.post,{$table_prefix}wpClassified_ads_subjects.ads_subjects_id, {$table_prefix}users.display_name, {$table_prefix}wpClassified_ads.date, {$table_prefix}wpClassified_ads.ads_id, {$table_prefix}wpClassified_ads.ads_ads_subjects_id, {$table_prefix}wpClassified_ads.post FROM {$table_prefix}wpClassified_lists, {$table_prefix}wpClassified_ads_subjects, {$table_prefix}wpClassified_ads,{$table_prefix}users WHERE {$table_prefix}wpClassified_lists.lists_id = {$table_prefix}wpClassified_ads_subjects.ads_subjects_list_id AND {$table_prefix}wpClassified_ads_subjects.ads_subjects_id = {$table_prefix}wpClassified_ads.ads_ads_subjects_id  AND {$table_prefix}users.id = {$table_prefix}wpClassified_ads.author ORDER BY {$table_prefix}wpClassified_lists.name, {$table_prefix}wpClassified_ads.date AND {$table_prefix}wpClassified_ads.status='active' DESC LIMIT 0, ".$limit;
+$sql = "SELECT {$table_prefix}wpClassified_lists.lists_id,{$table_prefix}wpClassified_lists.name, {$table_prefix}wpClassified_ads.subject, {$table_prefix}wpClassified_ads.status, {$table_prefix}wpClassified_ads.image_file, {$table_prefix}wpClassified_ads.post,{$table_prefix}wpClassified_ads_subjects.ads_subjects_id, {$table_prefix}wpClassified_ads.date, {$table_prefix}wpClassified_ads.ads_id, {$table_prefix}wpClassified_ads.ads_ads_subjects_id, {$table_prefix}wpClassified_ads.post FROM {$table_prefix}wpClassified_lists, {$table_prefix}wpClassified_ads_subjects, {$table_prefix}wpClassified_ads WHERE {$table_prefix}wpClassified_lists.lists_id = {$table_prefix}wpClassified_ads_subjects.ads_subjects_list_id AND {$table_prefix}wpClassified_ads_subjects.ads_subjects_id = {$table_prefix}wpClassified_ads.ads_ads_subjects_id AND {$table_prefix}wpClassified_ads.status='active'
+ORDER BY {$table_prefix}wpClassified_lists.name, {$table_prefix}wpClassified_ads.date DESC LIMIT 0, ".$limit;
 
 $posts = $wpdb->get_results($sql);
 
-		
 # Define Channel Elements
 $rssTitle=get_bloginfo('name').' - '.__("Classified");
 $rssLink = get_bloginfo('wpurl'). "/?page_id=". $pageinfo["ID"]. "&mp;_action=wpcfeed";
@@ -49,43 +53,24 @@ if($posts) {
 	}
 }
 
-$fp = fopen($filename, 'w');
-ob_start();
+$contents = '<?xml version="1.0" encoding="' .  get_settings('blog_charset') . "\"?>\n";
+$contents .= '<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" '.do_action('rss2_ns').">\n";
+$contents .= "<channel>\n<title>$rssTitle</title>\n";
+$contents .= "<link>" . WPC_PLUGIN_DIR . "/cache/wpclassified.xml</link>\n";
+$contents .= "<description>$rssDescription</description>\n";
+$contents .= "<generator>$rssGenerator</generator>\n";
+$contents .= "<language>" . get_option('rss_language') . "</language>\n";
+$contents .= "<pubDate>" . date("r") . "</pubDate>\n";
 
-/*
-if (empty($wp)) {
-	require_once('wp-config.php');
-	wp('feed=rss2');
-}
-*/
 
-?>
-<?php echo '<?xml version="1.0" encoding="' . get_settings('blog_charset') . '"?'.'>'; ?>
-<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" 
-xmlns:wfw="http://wellformedweb.org/CommentAPI/" 
-xmlns:dc="http://purl.org/dc/elements/1.1/" <?php do_action('rss2_ns'); ?>>
-
-<channel>
-	<title><?php wpcRssFilter($rssTitle);  ?></title>
-	<link><?php echo $wpClassified->plugin_url . '/cache/wpclassified.xml'; ?></link>
-	<description><?php wpcRssFilter($rssDescription) ?></description>
-	<generator><?php wpcRssFilter($rssGenerator) ?></generator>
-	<language><?php echo get_option('rss_language'); ?></language>
-	<pubDate><?php echo date("D, d-M-Y"); ?> 00:00:00 UTC</pubDate>
-
-<?php
-
-$contents = ob_get_clean();
+$filename = WPC_PLUGIN_DIR . '/cache/wpclassified.xml';
+$fp = fopen($filename, 'w') or die("can't open file");
 fwrite($fp, $contents);
 
-/*
-	<atom:link href="<?php echo wpcRssFilter($atomLink) ?>" rel="self" type="application/rss+xml" />
-*/
+do_action('rss2_head');
 
-?>
-<?php do_action('rss2_head'); ?>
-<?php foreach($rssItem as $item): ?>
-<?php ob_start(); start_wp();?>
+foreach($rssItem as $item): 
+  ob_start(); start_wp();?>
 	<item>
 		<title><?php echo $item->title ?></title>
 		<link><?php echo $item->guid ?></link>
@@ -104,12 +89,14 @@ fwrite($fp, $contents);
 		?>
 		<pubDate><?php echo $item->pubDate ?></pubDate>
 	</item>
-<?php
-	$contents = ob_get_clean();
-   fwrite($fp, $contents);
+  <?php
+    $contents = ob_get_clean();
+    fwrite($fp, $contents);
+
+endforeach;
+ob_start(); 
 ?>
-<?php endforeach; ?>
-<?php ob_start(); ?>
+
 </channel>
 </rss>
 <?php
