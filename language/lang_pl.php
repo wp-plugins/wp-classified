065e2a32a74dec3150e25cba5d780231b95df218
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-29 07:14:57 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/trunk@366155 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ language/lang_pl.php
@@ -0,0 +1,183 @@
+﻿<?php
+
+
+  // Translated 
+  // Web site:  
+  // create by: Mariusz Patalan
+
+  $lang=array();
+	//
+	$lang['_OR'] = "albo";
+	$lang['_CAT'] = "Kategoria :";
+	$lang['_CHARMAX'] = "Max znaków";
+	$lang['_SURNAME'] = "Imię i nazwisko:";
+	$lang['_VALIDATE'] = "Sprawdź";
+	$lang['_SELECTYPE'] = "Wybierz ogłoszenia typu";
+	$lang['_SELECTCAT'] = "Wybierz kategorię";
+	$lang['_SELECTCATOTHER'] = "Inna kategoria";
+	$lang['_RETURN'] = "Powrót";
+		
+	$lang['_INVALIDEMAIL'] = "Email jest wymagany.";
+	$lang['_INVALIDEMAIL2'] = "Proszę podać e-mail!";
+	$lang['_INVALIDNAME'] = "Musisz podać imię!";
+	$lang['_INVALIDSUBJECT'] = "Musisz podać temat!";
+	$lang['_INVALIDCOMMENT'] = "Opis jest wymagany.";
+	$lang['_INVALIDMSG'] = "Proszę poprawić ten błąd do zapisania.";
+	$lang['_INVALIDMSG2'] = "Obraz powinien być";
+	$lang['_INVALIDMSG3'] = "pixeli lub mniej.";
+	$lang['_INVALIDMSG4'] =  "Obraz powinien być mniejszy niż";
+	$lang['_INVALIDCONFIRM'] = "Kod potwierdzający nie pasuje do siebie.";
+	$lang['_INVALIDIMG'] = "Nieprawidłowy rozmiar obrazu.";
+	$lang['_YIMG'] = "Twój obraz ma";
+
+	$lang['_OPTIONAL'] = "opcjonalnie";
+	$lang['_REQUIRED'] = "wymagane";
+	$lang['_SAVEAD'] = "Zapisz ogłoszenie"; 
+
+	$lang['_DESC'] = "Opis:";
+	$lang['_PRINT'] = "Wydrukuj ogłoszenie";
+	$lang['_EMAIL'] = "Email:";
+	$lang['_TEL'] = "Telefon:";
+	$lang['_WEB'] = "Strona www:";
+	$lang['_CONFIRM'] = "Kod potwierdzenia:";
+	$lang['_PIC'] = "Obraz:";
+	$lang['_TITLE'] = "Tytuł"; // subject
+	$lang['_SUBJECT'] = "Temat"; //posting name
+	$lang['_AUTHOR'] = "Twoje imię i nazwisko/Nazwa firmy:";
+	$lang['_TEL'] = "Tel./ kom.:";
+	$lang['_DATE'] = "Data";
+	$lang['_LIST'] = "Lista";
+	$lang['_VIEWS'] = "Widok";
+	$lang['_ADS'] = "Ogłoszenie";
+	$lang['_LAST'] = "Ostatnie głoszenia";
+	$lang['_UPDATE'] = "Aktualizacja zakończona pomyślnie.";
+	$lang['_SEND'] = "Twoja wiadomość została wysłana. Dziękuje.";
+	$lang['_SENDNOTFAIL'] = "Wybrany email jest błędny";
+	$lang['_SENDNOT'] = "Wiadomość wybrana została wysłana.";
+	$lang['_ANNSEND'] = "Ogłoszenie zostało wysłane.";
+	$lang['_ANNDEL'] = "Ogłoszenie zostało usunięte.";
+	
+	$lang['_ANNMOD2'] = "Ogłoszenie zostało zmienione.";
+	$lang['_THANKS'] = "Dziękuje my za korzystanie z naszych Ogłoszeń.";
+	$lang['_MUSTLOGIN'] = "Musisz zalogować się aby dodawać ogłoszenie.";
+	$lang['_GUEST'] = "Nie jesteś zalogowany i piszesz jako Gość, kliknij";
+	$lang['_SENDEMAIL'] = "Wyślij email";
+	$lang['_SENDERR'] = "Wystąpił problem z wysłaniem poczty. Sprawdź czy poprawnie wypełniono formularz.";
+	$lang['_EXTRANN'] = "Te ogłoszenie jest z sekcji niejawnych ogłoszeń na stronie internetowej";
+	$lang['_FRIENDSEND'] = "<b> Wyślij ogłoszenie do znajomego </b><br> Możesz wysłać ogłoszenie nr ";
+	$lang['_SORRY'] = "Niestety, wydaje się że nie masz uprawnień wykonania danej czynności.";
+	$lang['_TOAFRIEND'] = " do znajomego.";
+	$lang['_YOURNAME'] = "Twoje imię i nazwisko:";
+	$lang['_YOUREMAIL'] = "Twój email:";
+	$lang['_SENDFR'] = "Wyślij";
+
+	$lang['_ADDANNONCE'] = "Dodaj nowe ogłoszenie w kategorii";
+	$lang['_YES'] = "Tak";
+	$lang['_NO'] = "Nie";
+	$lang['_BACK'] = "Wstecz";
+	$lang['_CANCEL'] = "Resetuj formularz";
+	$lang['_CONFDEL'] = "Potwierdzenie usunięcia ";
+	$lang['_SURDELANN'] = "UWAGA: Czy na pewno chcesz usunąć ogłoszenie?";
+	$lang['_ADSADDED'] = "Ta oferta została dodana";
+	$lang['_FROM'] = "Wprowadzone przez";
+	$lang['_CLASSIFIED_AD'] = "Ogłoszenie:";
+	$lang['_SEARCH'] = "Szukaj";
+	$lang['_VIEWALLADS'] = "Przeszukaj wszystkie ogłoszenia";
+
+	$lang['_YOURPHONE'] = "Twój Tel.:";
+	$lang['_YOURMESSAGE'] = "Twoja wiadomość:";
+	$lang['_VALIDMESS'] = "Wiadomość jest wymagana";
+	$lang['_MESSEND'] = "Twoja wiadomość została wysłana...";
+	$lang['_CLASSIFIED'] = "Ogłoszenia ";
+	$lang['_VIEW_MY_ADS'] = "Zobacz wszystkie ogłoszenia z ";	
+
+	$lang['_DELETE'] = "Usuń";
+	$lang['_EDITDESC'] = "Edytuj opis";
+	$lang['_DELETED'] = "Obraz został usunięty";
+	$lang['_EDIT'] = "Edytuj";
+	$lang['_SUBMITTER'] = "Imię i nazwisko:";
+	$lang['_ADD_LISTING'] = "Dodaj wpis";
+	$lang['_SUBMIT'] = "Wyślij";
+	
+	$lang['_ACTIVE'] = "Aktywne";
+	$lang['_INACTIVE'] = "Nie aktywne";
+
+	$lang['_FRIENDTITLE'] = "Wyślij ogłoszenie do znajomego";
+	$lang['_ADDETAIL'] = "Szczegóły ogłoszenia:";
+	$lang['_SENDTOF'] = "Wyślij to do znajomego";
+	$lang['_REPLY'] = "Odpowiedz na post";
+
+//// from 1.2.0-d
+
+	$lang['_HOW_LONG'] = "Jak długo ma być pokazywane:";
+	$lang['_DAY'] = " dni";
+	$lang['_CONTACTBY'] = "Po:";
+	$lang['_YES_CONTACT'] = "Tak";
+	$lang['_NO_CONTACT'] = "Nie";
+	$lang['_TERM'] = "Zgadzam się z regulaminem:";
+	
+	$lang['_FRIENDNAME'] = "Imię znajomego:";
+	$lang['_FRIENDMAIL'] = "Email znajomego:";
+
+$lang['_FRIENDBTN1'] = "Aby zobaczyć zdjęcie i więcej szczegółów, kliknij na poniższy link lub skopiuj go i wklej adres do okna przeglądarki.<br><br>";
+	$lang['_FRIENDBTN2'] = "<br><hr>Darmowe ogłoszenia ze zdjęciami.";
+
+//// from 1.2.0-g
+	$lang['_NEWADDURATION'] = "Czas trwania ogłoszenia i ważności";
+	$lang['_NEWADDEFAULT'] = "Nowe ogłoszenie domyślnie (dni):";
+	$lang['_SENDREMIDE'] = "Wyślij email z przypomnieniem przed upływem (dni):";
+	$lang['_NOTMESSAGE'] = "Zgłoszenie wiadomości:";
+	$lang['_NOTMESSAGESUBJECT'] = "Przypomnienie o wygaśnięciu z zastrzeżeniem email";
+	$lang['_NOTMESSAGEBODY'] = "Przypomnienie o wygaśnięciu emaila:";
+
+	$lang['_ADMAXLIMIT'] = "Limit długości standardowych ogłoszeń:";
+	$lang['_ADMAXLIMITTXT'] = "Maksymalna liczba znaków, aby umożliwić dodanie niejawnych ogłoszeń";
+
+	$lang['_AUTHORSEARCH'] = "Autor";	
+	$lang['_INVALIDTERM'] = "Sprawdź warunki korzystania i regulamin";
+
+//// from 1.2.0-h
+	$lang['_DATEFORMAT'] = "Format daty:";
+	$lang['_ALLOWRSS'] = "Zezwól na kanały RSS:";
+	$lang['_COMFCODE'] = "Pokaż kod potwierdzenia:";
+	$lang['_NOPOSTS'] = "Liczba najnowszych ogłoszeń na kanale:";
+
+//// from 1.2.1
+	$lang['_NEWPOST'] = "Informacja o nowych zatwierdzeniach ogłoszenia/recenzje";
+	$lang['_NOTIFYADMIN'] = "Zgłoszenia: Admin";
+	$lang['_NOTNOTIFY'] = "Nie został zgłoszony";
+	$lang['_POSTON'] = "Post w";
+	$lang['_SAVEADINFO'] = "Ogłoszenie zostało zapisane.";
+	$lang['_UPLOAD'] = "Wyślij";
+	$lang['_DELETESURE'] = "Czy na pewno chcesz usunąć?";
+
+// from 1.3.0-c
+	$lang['_TOCONTACTBY'] = "Aby skontaktować się przez e-mail skorzystaj z formularza kontaktowego na naszej stronie klikając na link e-mail w ogłoszeniu można zobaczyć reklamę na stronie internetowej:";
+	$lang['_ADLINKINFO'] = "Ta reklama jest z sekcji niejawnych ogłoszenia na stronie internetowej";
+	$lang['_ADDIMAGE'] = "Dodaj zdjęcie";
+	$lang['_DELIMAGE'] = "Usuń zdjęcie";
+
+// from 1.3.g
+
+	$lang['_INVALIDURL'] = "Proszę podać adres strony!";
+	$lang['_INVALIDPHONE'] = "Proszę podać numer telefonu!";
+
+// from 1.3.1-b
+	$lang['_INVALIDTITLE'] = "Tytuł powinien zawierać tylko litery i cyfry i pomiędzy 5 do 30 liter";
+	$lang['_MAIN'] = "Ogłoszenia";
+
+// from 1.3.1-b
+	$lang['_APPROVE'] = "Dziękujemy za korzystanie z naszych Ogłoszeń <br>Twoje ogłoszenie zostało pomyślnie przesłane do sprawdzenia <br> Zapraszam do przedstawienia innego ogłoszenia!";
+	$lang['_APPROVEREPLY'] = "To ogłoszenie zostało już przedstawione. Sprawdzimy je jak najszybciej się da.";
+
+	//$lang['_RETURNANN'] = "Wróć do wykazu ogłoszeń";
+	//$lang['_DU'] = "dodany w";   
+	//$lang['_SENDBY'] = "Dodany przez:";
+	//$lang['_NUMANNN'] = "Ogłoszenia nr. :";	
+	//$lang['_MESSAGE'] = "uważa, że te Ogłoszenie może Cię zainteresować i przekazał Ci tą wiadomość.";
+	//$lang['_INTERESS'] = "Inne ogłoszenia można zobaczyć w ";
+	//$lang['_BYMAIL'] = "Email :";
+	//$lang['_DISPO'] = "wygasa";
+	//$lang['_NOMAIL'] = "Nie dajemy adresów e-mail użytkowników, do kontaktu z nimi przez e-mail skorzystaj z formularza kontaktowego na naszej stronie klikając na link e-mail w ogłoszeniu. Możesz wyświetlić ogłoszenia na stronie internetowej.";
+
+?>
