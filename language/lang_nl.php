2501b78154d1f88db7b92225cbdfe872e585796e
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-01 12:54:42 +0000

    added Dutch translation
    git-svn-id: https://plugins.svn.wordpress.org/wp-classified/trunk@976431 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ language/lang_nl.php
@@ -0,0 +1,210 @@
+<?php
+// Translated 
+// Dutch translation for wpClassified Wordpress plugins Version: 1.4.2-c
+// define('WPLANG', 'nl_NL'); 
+// or
+// define('WPLANG', 'nl_BE');
+// Email: vertalingen@bvevc.nl
+
+    $lang=array();
+	//
+	$lang['_OR'] = "of";
+	$lang['_CAT'] = "Rubriek :";
+	$lang['_CHARMAX'] = "Maximale tekens";
+	$lang['_SURNAME'] = "Naam :";
+	$lang['_VALIDATE'] = "Valideren";
+	$lang['_SELECTYPE'] = "Selecteeer een Classified Adv. soort";
+	$lang['_SELECTCAT'] = "kies een rubriek";
+	$lang['_SELECTCATOTHER'] = "Andere rubriek";
+	$lang['_RETURN'] = "Terug";
+		
+	$lang['_INVALIDEMAIL'] = "Email is verplicht.";
+	$lang['_INVALIDEMAIL2'] = "Gebruik een geldig e-mail!";
+	$lang['_INVALIDNAME'] = "Je dient een naam in te vullen!";
+	$lang['_INVALIDSUBJECT'] = "Je dient een onderwerp in te vullen!";
+	$lang['_INVALIDCOMMENT'] = "Een omschrijving is een verplicht veld.";
+	$lang['_INVALIDMSG'] = "Verbeter de fouten om deze Adv. te plaatsen.";
+	$lang['_INVALIDMSG2'] = "De foto moet ";
+	$lang['_INVALIDMSG3'] = " pixels of lager. ";
+	$lang['_INVALIDMSG4'] =  "De foto dient minder te zijn dan ";
+	$lang['_INVALIDCONFIRM'] = "De veiligheidscode klopte niet..";
+	$lang['_INVALIDIMG'] = "Foute fotomaat.";
+	$lang['_YIMG'] = "Jouw foto was ";
+
+	$lang['_OPTIONAL'] = " optioneel. ";
+	$lang['_REQUIRED'] = " verplicht. ";
+	$lang['_SAVEAD'] = "Bewaar Adv."; 
+
+	$lang['_DESC'] = "Omschrijving:";
+	$lang['_PRINT'] = "Print deze Classified Adv";
+	$lang['_EMAIL'] = "Email:";
+	$lang['_TEL'] = "Telefoon:";
+	$lang['_WEB'] = "WebSite:";
+	$lang['_CONFIRM'] = "Veiligheids code:";
+	$lang['_PIC'] = "Foto:";
+	$lang['_TITLE'] = "Titel:"; // subject
+	$lang['_SUBJECT'] = "Onderwerp"; //posting name
+	$lang['_AUTHOR'] = "Jouw naam / Bedrijfsnaam:";
+	$lang['_TEL'] = "Telefoonnummer:";
+	$lang['_DATE'] = "Datum";
+	$lang['_LIST'] = "Rubriek";
+	$lang['_VIEWS'] = "Aantal x bezien";
+	$lang['_ADS'] = "Ads";
+	$lang['_LAST'] = "De nieuwste  ";
+	$lang['_UPDATE'] = "Je update was succesvol.";
+	$lang['_SEND'] = "Jouw mail is verzonden,  dank je..";
+	$lang['_SENDNOTFAIL'] = "De Email melding is niet gelukt.";
+	$lang['_SENDNOT'] = "De email is verzonden.";
+	$lang['_ANNSEND'] = "De advertentie is geselecteerd en verzonden.";
+	$lang['_ANNDEL'] = "De Advertentie is verwijderd.";
+	
+	$lang['_ANNMOD2'] = "Classified Adv is bewerkt.";
+	$lang['_THANKS'] = "Bedankt om Classified Ads te gebruiken.";
+	$lang['_MUSTLOGIN'] = "Je dient ingelogd te zijn om een adv. te kunnen plaatsen.";
+	$lang['_GUEST'] = "Je bent niet ingelogd, je post als een gast, klik";
+	$lang['_SENDEMAIL'] = "Verzend de mail";
+	$lang['_SENDERR'] = "Er is een probleem met de verzending. Kijk even na of de gegevens juist zijn ingevuld.";
+	$lang['_EXTRANN'] = "Dit is een advertentie van de website ";
+	$lang['_FRIENDSEND'] = "<b>Stuur deze advertentie naar een kennis</b><br>Je kan advertentie No. ";
+	$lang['_SORRY'] = "Sorry, het ziet er naar uit dat je geen toestemming hebt om dit te doen.";
+	$lang['_TOAFRIEND'] = " aan een vriend(in) zenden.";
+	$lang['_YOURNAME'] = "Jouw naam:";
+	$lang['_YOUREMAIL'] = "Jouw Email:";
+	$lang['_SENDFR'] = "Versturen";
+
+	$lang['_ADDANNONCE'] = '<span style="text-decoration:none; font-style:normal">Plaats jouw advertentie </span>';
+	$lang['_YES'] = "Ja";
+	$lang['_NO'] = "Nee";
+	$lang['_BACK'] = "Terug";
+	$lang['_CANCEL'] = "Herstel formulier";
+	$lang['_CONFDEL'] = "Bevestiging verwijdering";
+	$lang['_SURDELANN'] = "ATTENTIE : Weet je zeker dat je deze advertentie wilt verwijderen?";
+	$lang['_ADSADDED'] = "Deze Classified Adv. was toegevoegd door";
+	$lang['_FROM'] = "Ingezonden door ";
+	$lang['_CLASSIFIED_AD'] = "Classified Adv:";
+	$lang['_SEARCH'] = "Zoeken";
+	$lang['_VIEWALLADS'] = "Bezie alle Adv.";
+
+	$lang['_YOURPHONE'] = "Telefoon:";
+	$lang['_YOURMESSAGE'] = "Jouw bericht:";
+	$lang['_VALIDMESS'] = "Bericht is verplicht in te vullen.";
+	$lang['_MESSEND'] = "Jouw bericht werd verzonden...";
+	$lang['_CLASSIFIED'] = "Classified Adv ";
+	$lang['_VIEW_MY_ADS'] = "Bezie alle adv. van ";	
+
+	$lang['_DELETE'] = "Verwijder";
+	$lang['_EDITDESC'] = "bewerk omschrijving";
+	$lang['_DELETED'] = "Foto succesvol verwijderd";
+	$lang['_EDIT'] = "Bewerk";
+	$lang['_SUBMITTER'] = "Naam :";
+	$lang['_ADD_LISTING'] = "Voeg een lijst toe";
+	$lang['_SUBMIT'] = "Verzend";
+	
+	$lang['_ACTIVE'] = "Actief";
+	$lang['_INACTIVE'] = "Niet actief";
+
+	$lang['_FRIENDTITLE'] = "Stuur deze advertentie naar een vriend(in)";
+	$lang['_ADDETAIL'] = "Adv. details:";
+	$lang['_SENDTOF'] = "Stuur dit naar een vriend(in)";
+	$lang['_REPLY'] = "Reageer op bericht";
+
+//// from 1.2.0-d
+
+	$lang['_HOW_LONG'] = "Hoe lang wil je dat deze vertoont gaat worden:";
+	$lang['_DAY'] = " dagen ";
+	$lang['_CONTACTBY'] = "Toon je Emailadres:";
+	$lang['_YES_CONTACT'] = "Ja";
+	$lang['_NO_CONTACT'] = "Nee";
+	$lang['_TERM'] = "Gebruikersvoorwaarden:";
+	
+	$lang['_FRIENDNAME'] = "Vriend(in) Naam:";
+	$lang['_FRIENDMAIL'] = "Vriend(in) Email:";
+
+	$lang['_FRIENDBTN1'] = "Om meer fotos of tekst te zien, klik op de volgende link of kopier deze in je browser:<br><br>";
+	$lang['_FRIENDBTN2'] = "<BR><hr>Gratis classified adv. met fotos.";
+
+//// from 1.2.0-g
+	$lang['_NEWADDURATION'] = "Wat is de geldigheidsduur van de advertenties";
+	$lang['_NEWADDEFAULT'] = "Nieuwe advertenties blijven (dagen): ";
+	$lang['_SENDREMIDE'] = "Herinnering Email, aantal dagen vooraf: ";
+	$lang['_NOTMESSAGE'] = "Mededeling bericht:";
+	$lang['_NOTMESSAGESUBJECT'] = "Verval herinnering e-mail omschrijving";
+	$lang['_NOTMESSAGEBODY'] = "Verval herinnering e-mail inhoud:";
+
+	$lang['_ADMAXLIMIT'] = "Berichten lengte tekens in een advertentie:";
+	$lang['_ADMAXLIMITTXT'] = "Maxium aantal tekens toegestaan in een bericht.";
+
+	$lang['_AUTHORSEARCH'] = "Schrijver";	
+	$lang['_INVALIDTERM'] = "Controleer de gebruikersvoorwaarden";
+
+//// from 1.2.0-h
+	$lang['_DATEFORMAT'] = "Datum vertoning:";
+	$lang['_ALLOWRSS'] = "Toestaan RSS Feeds:";
+	$lang['_COMFCODE'] = "Toon de veiligheidscode:";
+	$lang['_NOPOSTS'] = "Aantal nieuwe adv. van de feedlijst:";
+
+//// from 1.2.1
+	$lang['_NEWPOST'] = "Meldingen van nieuwe adv. goedkeuring/bezien!";
+	$lang['_NOTIFYADMIN'] = "Gemeld: Administrateur";
+	$lang['_NOTNOTIFY'] = "Niet gemeld";
+	$lang['_POSTON'] = "Gepost op";
+	$lang['_SAVEADINFO'] = "De Classified Advertentie is bewaard.";
+	$lang['_UPLOAD'] = "Upload";
+	$lang['_DELETESURE'] = "Weet je zeker dit te willen verwijderen??";
+
+// from 1.3.0-c
+	$lang['_TOCONTACTBY'] = "Om contact te maken per e-mail gebruik daarvoor de maillink in de advertentie, je kan ook naar het volgende webadres om deze advertentie te zien:";
+	$lang['_ADLINKINFO'] = "Deze advertentie is van de classified adv. selectie in de website ";
+	$lang['_ADDIMAGE'] = "Fotos toevoegen";
+	$lang['_DELIMAGE'] = "Verwijder fotos";
+
+// from 1.3.g
+
+	$lang['_INVALIDURL'] = "Gelieve een goede en juiste URL op te geven!";
+	$lang['_INVALIDPHONE'] = "Gelieve een juist telefoonnummer in te vullen!";
+
+// from 1.3.1-b
+	$lang['_INVALIDTITLE'] = "De titel dient tussen de 5 en 30 tekens te zijn.";
+	$lang['_MAIN'] = "Begin";
+
+// from 1.3.1-b
+	$lang['_APPROVE'] = "Bedankt voor het gebruik van onze Classified Adv.<br>Jouw classified advertentie,  is doorgezonden voor controlle, bij goedkeuring zal deze spoedig gepost worden.<br>Je bent welkom om je volgende advertentie al te maken.";
+	$lang['_APPROVEREPLY'] = "Deze advertentie is al toegevoegd,  wij kijken dit na,  zo spoedig als mogelijk.";
+
+
+//
+   $lang['_DATEADDED'] = "Datum toegevoegd ";
+   $lang['_EDITADS'] = "Bewerk jouw advertentie";
+	//$lang['_RETURNANN'] = "Return classified ads listing";
+	//$lang['_DU'] = "added on";   
+	//$lang['_SENDBY'] = "Added by :";
+	//$lang['_NUMANNN'] = "Classified Artikel No. :";	
+	//$lang['_MESSAGE'] = "thinks this Classified Artikel might interest you and has sent you this message.";
+	//$lang['_INTERESS'] = "Other Classified Artikels can be seen at ";
+	//$lang['_BYMAIL'] = "Email :";
+	//$lang['_DISPO'] = "expires on";
+	//$lang['_NOMAIL'] = "We do not give out users email addresses, to contact them by e-mail please use the contact form on our site by clicking on the e-mail link in the ad, you can view the ad at the following web address. ";
+	$lang['_PREVIEW'] = "Voorvertoning ";
+	
+	$lang['_DETAILS'] = "Details";
+	$lang['_READ_MORE'] = "Lees verder";
+	$lang['_CHARS_LEFT'] = "tekens over";
+	$lang['_CHAR_MAX_OF'] = "maximum ";
+	$lang['_CHAR_MAX_ALLOWED'] = "maximaal aantal tekens toegestaan.";
+	$lang['_ADDMODDIMAGE'] = "Toevoegen / bewerken foto";
+	$lang['_EXPAND_ALL'] = "Uitklappen";
+	$lang['_COLLAPSE_ALL'] = "Inklappen";
+	$lang['_MUST_LOGIN'] = "Sorry, Je dient geregisteerd te zijn en ingelogd om een advertentie te zetten.";
+	$lang['_LOGIN'] = "Sorry, login hier";
+	$lang['_MAY_REGISTER'] = "Schrijf je hier in";
+	$lang['_AD_ID'] = "adv.";
+	$lang['_HERE'] = "hier";
+	$lang['_TO_LOGIN'] = "om in te loggen ";
+	$lang['_USER_ADS'] = "Adv. van deze gebruiker";
+	$lang['_PREVIEW'] = "Voorvertoning";
+	$lang['_PHONENO_EX'] = "Bijvoorbeeld: +32 1235-5678";
+	
+	
+	
+
+?>
